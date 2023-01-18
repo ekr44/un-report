b0b59ef703530b5434b1b361223c0ec64d7c2df8
ekr44 <ekr44@cornell.edu>
2023-01-18 14:05:52 -0500

    added gdp vs emissions plot

--- code/un_dataanalysis.R
+++ code/un_dataanalysis.R
@@ -1,8 +1,11 @@
 #Day 2 notes 
 #January 18th, 2023
 
-library(tidyverse)
-library(readr)
+library("tidyverse")
+library("readr")
+library("ggprism")
+library("extrafont")
+library("ggpubr")
 gapminder_data <- read_csv("data/gapminder_data.csv")
 summarize(gapminder_data, averageLifeExp=mean(lifeExp), medianLifeExp=median(lifeExp))
 
@@ -100,4 +103,41 @@ inner_join(co2_emissions, gapminder_data_2007, by = "country")
 anti_join(gapminder_data_2007, co2_emissions, by = "country")
 
 full_join(co2_emissions, gapminder_data_2007)%>%View()
-         
+
+co2_emissions%>%
+  left_join(gapminder_data_2007)
+
+joined_co2_pop<-inner_join(co2_emissions, gapminder_data_2007)         
+
+#Writing an object to a CSV
+write_csv(joined_co2_pop, file = "data/joined_co2_pop.csv")
+
+joined_co2_pop<-read_csv("data/joined_co2_pop.csv")
+
+View(joined_co2_pop)
+ggplot(joined_co2_pop)+
+  aes(x = gdpPercap)+
+  labs(x = "GDP per Capita", y = "Count")+
+  geom_histogram()+
+  theme_prism()
+
+ggplot(joined_co2_pop)+
+  aes(x = lifeExp)+
+  labs(x="Life Expectancy (Yrs)", y = "Count")+
+  geom_histogram()+
+  theme_prism()
+
+ggplot(joined_co2_pop)+
+  aes(x = per_cap_emissions)+
+  labs(x="CO2 emissions per capita", y = "Count")+
+  geom_histogram()+
+  theme_prism()
+
+gdp_co2_plote<-joined_co2_pop%>%
+  ggplot(aes(x = gdpPercap, y = per_cap_emissions))+
+  geom_point()+
+  geom_smooth(method = "lm", se = FALSE)+
+  labs(x = "GDP per capita", y = "CO2 emissions per capita", title = "CO2 emissions per capita vs GDP per capita")+
+  theme_prism()+
+  ggpubr::stat_regline_equation(aes(label = after_stat(rr.label)))
+ggsave(gdp_co2_plote, file = "figures/gdp_co2_plot.png", height = 4, width = 6, units = "in", dpi = 300)  
